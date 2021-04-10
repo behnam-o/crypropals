b1d575b46c03ab731f9daa5d7ecf7e68c0e8dc0d
Behnam Ousat <behnam.ousat@gmail.com>
2021-04-10 12:42:34 -0600

    new

--- set1.go
+++ set1.go
@@ -6,6 +6,7 @@ import (
 	hex "encoding/hex"
 	"fmt"
 	"os"
+	"strings"
 )
 
 const LOWER_LETTERS string = "abcdefghijklmnopqrstuvwxyz"
@@ -13,18 +14,60 @@ const UPPER_LETTERS string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const LETTERS string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const NUMBERS string = "0123456789"
 const PUNCTUATIONS string = ".!?,'\""
+const LETTER_COUNT = 26
 
-var english_freq = [26]float64{
-	0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, // A-G
-	0.06094, 0.06966, 0.00153, 0.00772, 0.04025, 0.02406, 0.06749, // H-N
-	0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, // O-U
-	0.00978, 0.02360, 0.00150, 0.01974, 0.00074} // V-Z
-
-func main() {
-	challenge5()
+var AVERAGE_FREQS = map[rune]float64{
+	'a': 0.08167,
+	'b': 0.01492,
+	'c': 0.02782,
+	'd': 0.04253,
+	'e': 0.12702,
+	'f': 0.02228,
+	'g': 0.02015,
+	'h': 0.06094,
+	'i': 0.06966,
+	'j': 0.00153,
+	'k': 0.00772,
+	'l': 0.04025,
+	'm': 0.02406,
+	'n': 0.06749,
+	'o': 0.07507,
+	'p': 0.01929,
+	'q': 0.00095,
+	'r': 0.05987,
+	's': 0.06327,
+	't': 0.09056,
+	'u': 0.02758,
+	'v': 0.00978,
+	'w': 0.02360,
+	'x': 0.00150,
+	'y': 0.01974,
+	'z': 0.00074,
 }
 
-func set1() {
+func main() {
+	fmt.Println(AVERAGE_FREQS)
+}
+
+func getletterdistributions(str string) {
+	str = strings.ToLower(str)
+	// freqs := generateEmptuLetterFreqMap();
+	for i := 0; i < len(str); i++ {
+		// freqs[];
+	}
+}
+
+
+func generateEmptuLetterFreqMap() map[rune]float64 {
+	var freqs = map[rune]float64{}
+	const start rune = 'a'
+	var i rune = 0
+	for i = 0; i < LETTER_COUNT; i++ {
+		freqs[start+i] = 0.0
+	}
+	return freqs
+}
+func challenge1() {
 	var line string = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
 	bytes, _ := hex.DecodeString(line)
 	encoded64 := b64.RawStdEncoding.EncodeToString(bytes)
@@ -32,7 +75,7 @@ func set1() {
 	fmt.Println(encoded64)
 }
 
-func set2() {
+func challenge2() {
 	var line1 string = "1c0111001f010100061a024b53535009181c"
 	var line2 string = "686974207468652062756c6c277320657965"
 	bytes1, _ := hex.DecodeString(line1)
